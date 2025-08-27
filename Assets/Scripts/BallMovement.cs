3583712e892103aff86ff6fbcf88e053ef7f427b
Facundo Gerry <facuto365pro@gmail.com>
2025-08-26 22:15:17 -0300

    THE BALL MOVES
    WE HAVE GAME!!!

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -20,11 +20,23 @@ public class BallMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D ballRigidbody2D;
     [SerializeField] private GameObject ball;
 
-    public float ballSpeed = 200f;
+    public float ballSpeed = 10000f;
+
+    private Collider2D wallLeft;
+    private Collider2D wallRight;
+    private Collider2D rooftop;
+    private Collider2D floorbot;
+    private Collider2D p1;
+    private Collider2D p2;
 
     private void Awake()
     {
-
+        wallLeft = goalLeft.GetComponent<Collider2D>();
+        wallRight = goalRight.GetComponent<Collider2D>();
+        rooftop = roof.GetComponent<Collider2D>();
+        floorbot = floor.GetComponent<Collider2D>();
+        p1 = player1.GetComponent<Collider2D>();
+        p2 = player2.GetComponent<Collider2D>();
     }
 
     void Start()
@@ -39,18 +51,62 @@ public class BallMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        /*collision.gameObject.SetActive(true);
-        if (collision.Equals(goalRight) || collision.Equals(goalLeft))
+        Rigidbody2D rigidbody2D = ballRigidbody2D;
+
+        if (collision.collider == wallLeft || collision.collider == wallRight)
         {
-            ball.transform.position = Vector3.zero;
+            ball.transform.position = Vector2.zero;
+            //Agregar una funcion para que espere 3 segundos y vuelva a sacar
+            KickOff();
         }
-        if ()
+
+        if (collision.collider == rooftop)
         {
+            if (rigidbody2D.velocity.x > 0)
+            {
+                rigidbody2D.velocity = ballSpeed * new Vector2(1, -1).normalized;
+            }
+            else
+            {
+                rigidbody2D.velocity = ballSpeed * new Vector2(-1, -1).normalized;
+            }
+        }
 
+        if (collision.collider == floorbot)
+        {
+            if (rigidbody2D.velocity.x > 0)
+            {
+                rigidbody2D.velocity = ballSpeed * new Vector2(1, 1).normalized;
+            }
+            else
+            {
+                rigidbody2D.velocity = ballSpeed * new Vector2(-1, 1).normalized;
+            }
+        }
 
+        if (collision.collider == p1)
+        {
+            if (rigidbody2D.velocity.y > 0)
+            {
+                rigidbody2D.velocity = ballSpeed * new Vector2(1, 1).normalized;
+            }
+            else
+            {
+                rigidbody2D.velocity = ballSpeed * new Vector2(1, -1).normalized;
+            }
+        }
 
-        }*/
-
+        if (collision.collider == p2)
+        {
+            if (rigidbody2D.velocity.y > 0)
+            {
+                rigidbody2D.velocity = ballSpeed * new Vector2(-1, 1).normalized;
+            }
+            else
+            {
+                rigidbody2D.velocity = ballSpeed * new Vector2(-1, -1).normalized;
+            }
+        }
     }
 
     public void KickOff()
@@ -60,12 +116,12 @@ public class BallMovement : MonoBehaviour
         {
             if (Random.value < 0.5f)
             {
-                rigidbody2D.AddForce(ballSpeed * Time.fixedDeltaTime * new Vector2(1, Random.value));
+                rigidbody2D.velocity = ballSpeed * new Vector2(1, Random.value).normalized;
                 Debug.Log("Saque a la DERECHA ARRIBA");
             }
             else
             {
-                rigidbody2D.AddForce(ballSpeed * Time.fixedDeltaTime * new Vector2(1, -Random.value));
+                rigidbody2D.velocity = ballSpeed * new Vector2(1, -Random.value).normalized;
                 Debug.Log("Saque a la DERECHA ABAJO");
             }
         }
@@ -73,12 +129,12 @@ public class BallMovement : MonoBehaviour
         {
             if (Random.value < 0.5f)
             {
-                rigidbody2D.AddForce(ballSpeed * Time.fixedDeltaTime * new Vector2(-1, Random.value));
+                rigidbody2D.velocity = ballSpeed * new Vector2(-1, Random.value).normalized;
                 Debug.Log("Saque a la IZQUIERDA ARRIBA");
             }
             else
             {
-                rigidbody2D.AddForce(ballSpeed * Time.fixedDeltaTime * new Vector2(-1, -Random.value));
+                rigidbody2D.velocity = ballSpeed * new Vector2(-1, -Random.value).normalized;
                 Debug.Log("Saque a la IZQUIERDA ABAJO");
             }
         }
